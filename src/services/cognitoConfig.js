1ab13646aa57953b88cfa13995b2f05873681286
Brian Smith <briaguya@gmail.com>
2017-06-06 00:49:43 -0500

    Fixing config

--- src/services/cognitoConfig.js
+++ src/services/cognitoConfig.js
@@ -1,6 +1,6 @@
 export default {
-  region: 'us-east-1',
-  IdentityPoolId: 'us-east-1:a800952f-04b4-4413-8cf3-802c23e0973c',
-  UserPoolId: 'us-east-1_ouAxSAN2I',
-  ClientId: '14h066okfibmp3tfaskjkevktv',
+  region: process.env.COGNITO_REGION,
+  IdentityPoolId: process.env.COGNITO_IDENTITY_POOL_ID,
+  UserPoolId: process.env.COGNITO_USER_POOL_ID,
+  ClientId: process.env.COGNITO_CLIENT_ID,
 }
