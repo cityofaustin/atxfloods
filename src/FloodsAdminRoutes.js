c9498e0afa53d19f3b8aa8f19a0a7940890bc7db
Brian Smith <briaguya@gmail.com>
2017-06-27 23:00:30 -0500

    adding status updates works, but it's kinda messy right now
    when adding a status update, it doesn't invalidate the cache around crossings, so the list crossings page doesn't update immediately.
    It also doesn't set the state once the options load in, I decided not to spend time fighting this one because we aren't going to use a dropdown to select crossings to update

--- src/FloodsAdminRoutes.js
+++ src/FloodsAdminRoutes.js
@@ -9,6 +9,7 @@ import PrivateRoute from './PrivateRoute';
 import AuthButton from './AuthButton';
 import CreateUser from './CreateUser';
 import CrossingList from './CrossingList';
+import NewStatusUpdate from './NewStatusUpdate';
 import auth from './services/gqlAuth';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
@@ -25,11 +26,13 @@ class FloodsAdminRoutes extends Component {
             <li><Link to="/crossinglist">List Crossings (Public)</Link></li>
             <li><Link to="/protected">Protected Page</Link></li>
             <li><Link to="/createuser">Create User (Protected)</Link></li>
+            <li><Link to="/updatestatus">Update Crossing Status (Protected)</Link></li>
           </ul>
           <Route path="/public" component={Public}/>
           <Route path="/crossinglist" component={CrossingList}/>
           <Route path="/login" component={Login}/>
           <PrivateRoute path="/protected" component={Protected} authenticated={auth.isAuthenticated()}/>
+          <PrivateRoute path="/updatestatus" component={NewStatusUpdate} authenticated={auth.isAuthenticated()}/>
           <PrivateRoute path="/createuser" component={CreateUser} authenticated={auth.isAuthenticated()}/>
         </div>
     );
